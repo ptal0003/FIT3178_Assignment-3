d83dcdf20b7f6036e4faa249c78e35e320f2df40
Prateek Talukdar <ptal0003@student.monash.edu>
2021-05-14 13:52:06 +0530

    Added Collection Views to Professor Section, adding the Other Books By Author Section

---
+++ Assignment 3/BookCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  BookCollectionViewCell.swift
+//  Assignment 3
+//
+//  Created by Jyoti Talukdar on 14/05/21.
+//
+
+import UIKit
+
+class BookCollectionViewCell: CollectionViewCell {
+
+}
