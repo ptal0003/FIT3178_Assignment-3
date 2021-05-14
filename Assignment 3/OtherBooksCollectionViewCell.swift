4003acbb5aa86141b6d068ffa7b17def27d98c4c
Prateek Talukdar <ptal0003@student.monash.edu>
2021-05-14 20:10:28 +0530

    More From the Author Functionality Added

---
+++ Assignment 3/OtherBooksCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  OtherBooksCollectionViewCell.swift
+//  Assignment 3
+//
+//  Created by Jyoti Talukdar on 14/05/21.
+//
+
+import UIKit
+
+class OtherBooksCollectionViewCell: UICollectionViewCell {
+    
+}
